3574e401c1a60db0d210665552b9f526bf0c0124
Adel Shurrab <143008607+Adel-Shurrab@users.noreply.github.com>
2026-03-13 07:51:49 +0200

    js file

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("btn").addEventListener("click", () => {
+    document.getElementById("msg").innerText = "Hello from JavaScript! The CI/CD Pipeline is successfully deployed. OK!";
+    document.getElementById("msg").style.color = "green";
+    document.getElementById("msg").style.fontWeight = "bold";
+});
